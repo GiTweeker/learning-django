048f27ecc70c4000e092c22936f40242585ea85b
Edih Elijah <richkingpere@gmail.com>
2016-09-06 17:41:23 +0100

    ep22

---
+++ music/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+
+    def __str__(self):
+
+        return 'The album name is : ' +  self.album_title + ' The artist name is : ' + self.artist + '\n'
+
+
+
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
+    is_favorite = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return 'The title of the song is : ' + self.song_title
---
+++ music/views.py
@@ -0,0 +1,15 @@
+
+from .models import Album
+from django.shortcuts import render, get_object_or_404
+def index(request):
+    all_albums = Album.objects.all()
+    context = {'all_albums':all_albums }
+    return render(request,'music/index.html',context)
+
+def detail(request,album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album':album})
+
+
+def favorite(request):
+    return None
