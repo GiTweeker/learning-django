1beaa12bba547613caffb63b04768f566d866e41
Edih Elijah <richkingpere@gmail.com>
2016-09-07 09:01:53 +0100

    ep24

--- music/views.py
+++ music/views.py
@@ -1,5 +1,4 @@
-
-from .models import Album
+from .models import Album, Song
 from django.shortcuts import render, get_object_or_404
 def index(request):
     all_albums = Album.objects.all()
@@ -11,5 +10,15 @@ def detail(request,album_id):
     return render(request, 'music/detail.html', {'album':album})
 
 
-def favorite(request):
-    return None
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html',
+                      {'album': album, 'error_message': 'Invalid song selectd'})
+
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
