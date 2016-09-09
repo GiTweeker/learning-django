8ac4ca14b1568dc3adc0b13f1f9ba99e0287c9fb
Edih Elijah <richkingpere@gmail.com>
2016-09-09 21:29:14 +0100

    eps31

--- music/views.py
+++ music/views.py
@@ -1,25 +1,22 @@
-from .models import Album, Song
-from django.shortcuts import render, get_object_or_404, redirect
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums':all_albums }
-    return render(request,'music/index.html',context)
+from django.views import generic
+from django.views.generic import CreateView
 
-def detail(request,album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album':album})
+from .models import Album
 
 
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html',
-                      {'album': album, 'error_message': 'Invalid song selectd'})
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'albums'
 
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
+    def get_queryset(self):
+        return Album.objects.all()
 
-        return redirect('/music/' + album_id + '/')
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
